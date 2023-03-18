d714f1fe2b6aad2ad6b9108f3390fb2a3f2ec6f9
Dima Dobronovich <dobronovich_95@mail.ru>
2023-03-18 11:39:51 +0300

    main

---
+++ script.js
@@ -0,0 +1,27 @@
+let popap = document.querySelector('.popap');
+let btnEdit = document.querySelector('.profile__btn_edit');
+let close = document.querySelector('.popap__close');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let nameInput = document.getElementById('inputName');
+let jobInput = document.getElementById('inputJob');
+
+btnEdit.addEventListener('click', function () {
+  popap.classList.add('popap_active');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
+close.addEventListener('click', function () {
+  popap.classList.remove('popap_active');
+});
+
+
+let formElement = document.querySelector('.popap__form');
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popap.classList.remove('popap_active');
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
