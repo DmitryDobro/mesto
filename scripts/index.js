8a3d7c7a7782cdc5a0374f2bb743062fba9beedf
Dima Dobronovich <dobronovich_95@mail.ru>
2023-03-29 11:49:31 +0300

    pravka

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,42 @@
-let popap = document.querySelector('.popap');
-let btnEdit = document.querySelector('.profile__btn_edit');
-let close = document.querySelector('.popap__close');
+let popap = document.querySelector('.popup');
+let btnEdit = document.querySelector('.profile__btn_type_edit');
+let close = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let nameInput = document.getElementById('inputName');
 let jobInput = document.getElementById('inputJob');
+let formElement = document.querySelector('.popup__form');
+let heartsLinks = document.querySelectorAll('.cards__like');
+let cardsBlock = document.querySelector('.cards');
+
+function removeClass(selector, className){
+selector.classList.remove(className);
+}
 
 btnEdit.addEventListener('click', function () {
-  popap.classList.add('popap_active');
+  popap.classList.add('popup_active');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
 close.addEventListener('click', function () {
-  popap.classList.remove('popap_active');
+  removeClass(popap, 'popup_active')
 });
 
 
-let formElement = document.querySelector('.popap__form');
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popap.classList.remove('popap_active');
+  removeClass(popap, 'popup_active')
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 
 
-let heartsLinks = document.querySelectorAll('.cards__likes');
-let cardsBlock = document.querySelector('.cards');
 
-cardsBlock.addEventListener('click', ()=>{
-  if(event.target.closest('.cards__likes')){
-    event.target.closest('.cards__likes').classList.toggle('cards__likes_colorFon_black')
+cardsBlock.addEventListener('click', () => {
+  if (event.target.closest('.cards__like')) {
+    event.target.closest('.cards__like').classList.toggle('cards__like_color-fon_black');
   }
-})
+});
+
